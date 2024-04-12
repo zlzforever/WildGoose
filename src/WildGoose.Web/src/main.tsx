a92de87c290da80d05a0ef7a388c3cfb17663324
Lewis Zou <zlzforever@163.com>
2024-04-12 23:13:14 +0800

    解决相对路径导致 OICD 加载异常

--- src/WildGoose.Web/src/main.tsx
+++ src/WildGoose.Web/src/main.tsx
@@ -1,46 +1,41 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-const signinRedirectCallbackPath = "/signin-redirect-callback";
-const signinSilentCallbackPath = "/signin-silent-callback";
-import {
-  getUser,
-  signinRedirect,
-  signinRedirectCallback,
-  signinSilentCallback,
-} from "./lib/auth";
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { BrowserRouter } from "react-router-dom"
+const signinRedirectCallbackPath = "/signin-redirect-callback"
+const signinSilentCallbackPath = "/signin-silent-callback"
+import { getUser, signinRedirect, signinRedirectCallback, signinSilentCallback } from "./lib/auth"
 
 try {
-  let user;
-  let oidcCallback;
-  if (location.pathname === signinRedirectCallbackPath) {
-    user = await signinRedirectCallback();
-    oidcCallback = true;
-  } else if (location.pathname === signinSilentCallbackPath) {
-    user = await signinSilentCallback();
-    oidcCallback = true;
+  let user
+  let oidcCallback
+  if (location.pathname.endsWith(signinRedirectCallbackPath)) {
+    user = await signinRedirectCallback()
+    oidcCallback = true
+  } else if (location.pathname.endsWith(signinSilentCallbackPath)) {
+    user = await signinSilentCallback()
+    oidcCallback = true
   } else {
-    user = await getUser();
+    user = await getUser()
     if (user == null) {
-      await signinRedirect();
+      await signinRedirect()
     }
   }
   if (oidcCallback) {
-    let redirectPath = localStorage.getItem("RedirectPath");
-    redirectPath = redirectPath ? redirectPath : "/";
+    let redirectPath = localStorage.getItem("RedirectPath")
+    redirectPath = redirectPath ? redirectPath : "/"
     if (location.pathname != redirectPath) {
-      window.location.href = redirectPath;
+      window.location.href = redirectPath
     }
   } else {
-    localStorage.setItem("RedirectPath", "");
+    localStorage.setItem("RedirectPath", "")
   }
 } catch (error) {
-  await signinRedirect();
+  await signinRedirect()
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+)
