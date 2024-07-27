2089032bcb30f2cc8dfaa195198e19524e1e2b3a
Lewis Zou <zlzforever@163.com>
2024-07-27 08:27:40 +0800

    重新组织文件替换脚本

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,13 @@
 set -eu
 
 # 检查输入文件是否存在
-if [ ! -f "${CONFIG_SOURCE}" ]; then
-    echo "未设置配置源文件或源源文件不存在"
-    exit 1
+if [ -f "${CONFIG_SOURCE}" ]; then
+   # 读取文件内容，并替换环境变量
+   while IFS= read -r line; do
+       eval "echo \"$line\""
+   done <"${CONFIG_SOURCE}" >"/app/appsettings.json"
+   echo "应用配置文件已生成"
 fi
 
-# 读取文件内容，并替换环境变量
-while IFS= read -r line; do
-    eval "echo \"$line\""
-done <"${CONFIG_SOURCE}" >"/app/appsettings.json"
+echo "未设置配置源文件或源文件不存在"
+
