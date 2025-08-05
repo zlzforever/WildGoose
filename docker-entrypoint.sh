122d196a081b673885a58559a462bdab250b8e2d
Lewis Zou <zlzforever@163.com>
2025-08-05 19:43:20 +0800

    添加 dapr 下载

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,6 +19,20 @@ else
    echo "使用默认配置文件"
 fi
 
+# 检查环境变量 DAPR_URL 是否存在且不为空
+if [ -n "$DAPR_URL" ]; then
+    echo "检测到 DAPR_URL， 正在下载内容..."
+    # 使用 curl 下载 URL 内容
+    curl -O "$DAPR_URL"
+    # 检查 curl 命令是否执行成功
+    if [ $? -eq 0 ]; then
+        echo -e "\n下载 daprd 完成"
+        chmod +x daprd
+    else
+        echo -e "\n下载 daprd 失败"
+    fi
+fi
+
 exec "$@"
 
 
