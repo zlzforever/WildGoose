eedd6001fd407dff749c7979ef5e7e2a38871996
Lewis Zou <zlzforever@163.com>
2026-03-05 20:21:33 +0800

    修复禁止密码模式用户存储不正确

--- src/WildGoose.Application/NoPasswordStore.cs
+++ src/WildGoose.Application/NoPasswordStore.cs
@@ -5,20 +5,20 @@ using WildGoose.Infrastructure;
 namespace WildGoose.Application;
 
 public class NoPasswordStore(WildGooseDbContext context, IdentityErrorDescriber describer = null)
-    : UserStore<Domain.Entity.User>(context, describer)
+    : UserStore<Domain.Entity.User, Domain.Entity.Role, WildGooseDbContext>(context, describer)
 {
-    public override Task SetPasswordHashAsync(Domain.Entity.User user, string passwordHash, CancellationToken ct)
+    public override Task SetPasswordHashAsync(Domain.Entity.User user, string passwordHash, CancellationToken _)
     {
         // 不存密码
         return Task.CompletedTask;
     }
 
-    public override Task<string> GetPasswordHashAsync(Domain.Entity.User user, CancellationToken ct)
+    public override Task<string> GetPasswordHashAsync(Domain.Entity.User user, CancellationToken _)
     {
         return Task.FromResult(string.Empty);
     }
 
-    public override Task<bool> HasPasswordAsync(Domain.Entity.User user, CancellationToken ct)
+    public override Task<bool> HasPasswordAsync(Domain.Entity.User user, CancellationToken _)
     {
         // ✅ 核心：告诉系统：用户没有密码
         return Task.FromResult(false);
